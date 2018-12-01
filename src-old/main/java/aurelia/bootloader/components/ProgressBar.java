75caa6098c431183ab9a39d0da1a22c56cf69186
loucass003 <loucass003@gmail.com>
2018-12-01 18:17:37 +0100

    Initial commit

---
+++ src-old/main/java/aurelia/bootloader/components/ProgressBar.java
@@ -0,0 +1,36 @@
+package aurelia.bootloader.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProgressBar extends JPanel {
+
+    public double value;
+
+    public ProgressBar()
+    {
+        this.setLayout(null);
+        this.setOpaque(false);
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double v) {
+        value = v;
+    }
+
+    protected void paintComponent(Graphics g) {
+        double barRectWidth = (this.getWidth() * this.getValue()) / 100;
+
+        g.setColor(getBackground());
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+        int end = (int) barRectWidth;
+        g.setColor(getForeground());
+        g.fillRect(0, 0, end, this.getHeight());
+
+        repaint();
+    }
+}
